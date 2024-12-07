2bf4ae0cfe848ea388f7ae358ecc5064cab37104
nitin-veera <nitinveera@ucla.edu>
2024-12-07 05:30:41 -0800

    added bio and links

---
+++ components/main/Left.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+// import Nav from "../sub/Nav";
+import Links from "../sub/Links";
+
+const Left = () => {
+
+    return (
+        <div className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
+            <div>
+                <div className="text-4xl sm:text-5xl font-bold tracking-tight text-slate-200">
+                    Nitin Veeraperumal
+                </div>
+                    <div className="font-medium tracking-tight text-slate-200 sm:text-xl text-nowrap mt-4">
+                        Math and Computer Science @ UCLA
+                    </div>
+                <div className="mt-2 leading-normal whitespace-pre">
+                    Previously @ Grindr | Seeking Full Time Positions
+                </div>
+                {/*<div className="lg:mr-[70px] mt-4 md:mt-8 leading-relaxed">*/}
+                {/*    <div className="mb-4">*/}
+                {/*        to*/}
+                {/*    </div>*/}
+                {/*    <div className="mb-4">*/}
+                {/*        do*/}
+                {/*    </div>*/}
+                {/*    <div>*/}
+                {/*        later*/}
+                {/*    </div>*/}
+                {/*</div>*/}
+                {/*<Nav />*/}
+            </div>
+            <Links />
+        </div>
+    );
+};
+
+export default Left;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-
+import Left from "../../components/main/Left";
 export default function Home() {
   return (
     <div className="mx-auto min-h-screen max-w-screen-xl px-6 py-12 md:px-12 md:py-20 lg:px-24 lg:py-0">
       <div className="lg:flex lg:gap-4 z-0">
-        <h1>hi</h1>
+        <Left/>
       </div>
     </div>
   );
